30bf22668d31cbacc7bf2925cfb65d7a74992b7a
indridxcold <indridxcold4@gmail.com>
2024-06-06 21:05:02 -0700

    add shading button and function which reduces opacity incrementally by 0.1

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ let container = document.querySelector("#container");
 container.style.width = '960px'
 container.style.height = '960px'
 
-//creates the squares that make up the grid
-// need to create another function so there is not so much repetition between makeDivs
-// and makeDivsColor...too messy
+// need to create another function so there is not so much repetition between makeDivs and makeDivsColor...too messy
 
+//creates the squares that make up the grid
 function makeDivs() {
         const measurement = (920 - (userNum * 4)) /(userNum);
         for (let i = 0; i < userNum; i++) {
@@ -17,16 +16,48 @@ function makeDivs() {
                 squares.style.height = `${measurement}px`;
                 squares.style.width = `${measurement}px`;
                 squares.classList.add('squares');
-                
-                //adds event listener that assigns a random color 
+               
+
+                //adds event listener that assigns a grey color
                 squares.addEventListener("mouseover", function (e) {
-                    e.currentTarget.style.backgroundColor = "grey"
+                    e.currentTarget.style.backgroundColor = "rgba(194, 194, 194)";
+
                 }   
             );
                 boxOfSq.appendChild(squares);}
                 container.appendChild(boxOfSq);
         };
 };
+
+function makeDivsSketch() {
+    const measurement = (920 - (userNum * 4)) /(userNum);
+    for (let i = 0; i < userNum; i++) {
+        boxOfSq = document.createElement("div");
+        boxOfSq.classList.add('boxOfSq');
+            for (let sqRun = 0; sqRun < userNum; sqRun++){
+            squares = document.createElement("div");
+            squares.style.height = `${measurement}px`;
+            squares.style.width = `${measurement}px`;
+            squares.classList.add('squares');
+            squares.classList.add('sketch');
+            squares.style.opacity = 1;
+            
+            //adds event listener that assigns a grey color 
+            squares.addEventListener("mouseover", function (e) {
+                e.currentTarget.style.backgroundColor = "rgba(194, 194, 194)";
+                //incrementally reduces opacity by .1 until at zero. This effectively darkens the squares
+                // because the background of the container is black. As squares become more transparent, more background
+                //can be seen
+                    if (Number(e.currentTarget.style.opacity) > 0) {
+                        e.currentTarget.style.opacity = (Number(e.currentTarget.style.opacity) - 0.1);
+                    }
+            }   
+        );
+            boxOfSq.appendChild(squares);}
+            container.appendChild(boxOfSq);
+    };
+};
+
 function makeDivsColor() {
     const measurement = (920 - (userNum * 4)) /(userNum);
     for (let i = 0; i < userNum; i++) {
@@ -55,7 +86,6 @@ function makeDivsColor() {
 };
 
 makeDivs();
-console.log(squares);
 
 // random colors button
 let colorsBtn = document.querySelector("#randomColorsBtn");
@@ -83,6 +113,16 @@ let resetBtn = document.querySelector("#resetBtn");
         makeDivs();
 
 });
+
+let opacBtn = document.querySelector("#opacityBtn");
+    opacBtn.addEventListener("click", (event) => {
+        while (container.firstChild) {
+            container.firstChild.remove();
+        }
+        makeDivsSketch();
+        
+    
+    })
 
 
 
